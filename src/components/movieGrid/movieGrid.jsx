3fb458bc2d65ab0cdd7f0788cfa09da8a0903dd0
Akshat Gupta <akshatgupta300@gmail.com>
2024-02-05 20:20:19 +0530

    added next image tags for optimization

--- src/components/movieGrid/movieGrid.jsx
+++ src/components/movieGrid/movieGrid.jsx
@@ -1,15 +1,20 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function MovieGrid({heading, listData}) {
   return (
     <div className="flex flex-col gap-[20px]">
       <span className="font-bold text-[36px] md:text-[48px]">{heading}</span>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[50px] items-center">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-y-5 lg:gap-y-8">
         {listData.map(movie => (
-          <Link key={movie.id} href={`/movie/${movie.id}`}>
+          <Link className="flex justify-center" key={movie.id} href={`/movie/${movie.id}`}>
             {movie.poster_path ?
-            <img className="onHover w-[240px] m-auto" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} title={movie.title} /> :
-            <img className="onHover w-[240px] m-auto" src={`/no_poster.png`}  />
+              <div className="relative onHover w-[150px] h-[225px] lg:w-[220px] lg:h-[330px]">
+                <Image fill className="object-cover" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} title={movie.title}/>
+              </div> :
+              <div className="relative onHover w-[220px] h-[330px]">
+                <Image fill className="object-cover" src={`/no_poster.png`} alt={movie.title} title={movie.title}/>
+              </div>
             }
           </Link>
         ))}
--- src/components/movieList/movieList.jsx
+++ src/components/movieList/movieList.jsx
@@ -1,15 +1,20 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function MovieList({heading, listData}){
   return (
   <div className="flex flex-col gap-[20px]">
     <span className="font-bold text-[36px] md:text-[48px]">{heading}</span>
-    <div className="noScrollbar flex p-6 gap-[50px] overflow-x-scroll">
+    <div className="noScrollbar flex p-6 gap-5 lg:gap-[50px] overflow-x-scroll">
       {listData.map(movie => (
         <Link key={movie.id} className="onHover" href={`/movie/${movie.id}`}>
           {movie.poster_path!=null ?
-          <img className="max-w-[200px] md:max-w-[240px]" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} title={movie.title} /> :
-          <img className="max-w-[200px] md:max-w-[240px]" src={`/no_poster.png`} alt={movie.title} title={movie.title} />
+            <div className="relative w-[150px] h-[225px] lg:w-[220px] lg:h-[330px]">
+              <Image fill className="object-cover" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} title={movie.title}/>
+            </div> :
+            <div className="relative w-[150px] h-[225px] lg:w-[220px] lg:h-[330px]">
+              <Image fill className="object-cover" src={`/no_poster.png`} alt={movie.title} title={movie.title}/>
+            </div>
           }
         </Link>
       ))}
