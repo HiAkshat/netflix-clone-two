054a60edb64a871bac724c8fa2f8f76d7188bdab
Akshat Gupta <akshatgupta300@gmail.com>
2024-02-08 10:34:20 +0530

    movie grid optimization

--- src/components/movieGrid/movieGrid.jsx
+++ src/components/movieGrid/movieGrid.jsx
@@ -16,11 +16,11 @@ export default function MovieGrid({heading, listData}) {
           }} key={movie.id}>
             <Link className="flex justify-center" href={`/movie/${movie.id}`}>
               <div className="relative onHover w-[150px] h-[225px] lg:w-[220px] lg:h-[330px] rounded-xl">
-                <Skeleton className="absolute object-cover opacity-20  w-[150px] h-[225px] lg:w-[220px] lg:h-[330px] rounded-xl" />
+                <Skeleton className="absolute object-cover opacity-20  w-[150px] h-[225px] lg:w-[220px] lg:h-[330px] rounded-xl z-[-1]" />
                 {movie.poster_path ?
-                <Image fill className="object-cover rounded-xl" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} title={movie.title}/>
+                <Image width={220} height={330} className="object-cover rounded-xl" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} title={movie.title}/>
                 :
-                <Image fill className="object-cover rounded-xl" src={`/no_poster.png`} alt={movie.title} title={movie.title}/>
+                <Image width={220} height={330} className="object-cover rounded-xl" src={`/no_poster.png`} alt={movie.title} title={movie.title}/>
                 }
               </div>
             </Link>
